a231ac678d0158bb8a51dced0b510fdf3c2a7519
Lenard Pratt <striderman34@gmail.com>
2018-01-14 14:21:02 +0000

    Moved depende

--- compiler/src/lib.rs
+++ compiler/src/lib.rs
@@ -1,7 +1,14 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+extern crate llvm;
+extern crate syntax;
+
+use llvm::{Module,Context,Builder};
+
+
+fn compile(ast:&[WithPos<Statement>]) {
+
+    let ctx = Context::new();
+    let module = Module::new("main",&ctx);
+
+    
+
 }
