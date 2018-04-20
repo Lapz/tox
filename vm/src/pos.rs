e3283f9054a89f075353723258a63ab8459d0f88
Rowlandson Pratt <striderman34@gmail.com>
2018-04-20 15:55:18 +0100

    Add line encoding and ability to use ints and doubles

---
+++ vm/src/pos.rs
@@ -0,0 +1,24 @@
+/// Each of these marks the beginning of a new source line
+use std::fmt::{Display,self};
+use util::pos::Span;
+
+#[derive(PartialEq,Debug,Copy,Clone)]
+pub struct LineStart {
+    pub offset:usize,
+    pub line:u32,
+}
+
+impl Display for LineStart {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.line)
+    }
+}
+
+impl LineStart {
+    pub fn new(pos:Span) -> Self {
+        LineStart {
+            offset:pos.end.absolute,
+            line:pos.end.line,
+        }
+    }
+}
---
+++ vm/src/value.rs
@@ -0,0 +1,5 @@
+#[derive(Debug)]
+pub enum Value {
+    Int(i64),
+    Float(f64),
+}
