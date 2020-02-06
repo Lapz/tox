fed77364ae292c473675ba2750333d42bbe7e423
Lenard Pratt <l3np27@gmail.com>
2020-02-06 15:57:18 +0000

    Add test for if exprs

--- parser/src/parser/expressions/if_expr.rs
+++ parser/src/parser/expressions/if_expr.rs
@@ -27,10 +27,20 @@ where
 
     fn parse_else(&mut self) {
         self.bump(); // eat the `else`
+        self.skip_whitespace();
         if self.current() == T![if] {
+            println!("a");
             self.parse_if_expr()
         } else {
             self.parse_block();
         }
     }
+}
+
+#[cfg(test)]
+mod test {
+    test_parser! {parse_empty_if,"fn main() { if true {}}"}
+    test_parser! {parse_if_and_else,"fn main() { if true {} else {}}"}
+    test_parser! {parse_chained_if,"fn main() { if true {} else if false {} else if true {} else if false {} }"}
+    test_parser! {parse_chained_if_and_else,"fn main() { if true {} else if false {} else if true {} else {} }"}
 }
