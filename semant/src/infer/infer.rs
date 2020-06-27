e698e4c201b59d43910fc5a00a678af9e6a9d012
Lenard Pratt <l3np27@gmail.com>
2020-06-27 15:23:12 +0100

    Work

--- semant/src/infer/infer.rs
+++ semant/src/infer/infer.rs
@@ -7,6 +7,13 @@ struct InferDataCollector<DB> {
     resolver: Resolver,
 }
 
+
+impl <'a,DB> InferDataCollector<&'a DB> where DB:HirDatabase {
+    fn infer_function(&mut self,function:&Function) {
+        
+    }
+}
+
 pub fn infer_query(db: &impl HirDatabase, file: FileId) -> WithError<()> {
     let program = db.lower(file)?;
     let resolver = db.resolve_source_file(file)?;
@@ -14,6 +21,7 @@ pub fn infer_query(db: &impl HirDatabase, file: FileId) -> WithError<()> {
     let collector = InferDataCollector { db, resolver };
 
     for function in &program.functions {  
+        collector.infer_function(function);
         //
     }
 
